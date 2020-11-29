846e28685f9215f53b43f5138aeb08ecf0433395
liulong <liulong@liulongdeMacBook-Pro.local>
2020-11-29 23:30:38 +0800

    shapes test

---
+++ shapes/shpes.go
@@ -0,0 +1,28 @@
+package shapes
+
+import "math"
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.width * rectangle.height
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
