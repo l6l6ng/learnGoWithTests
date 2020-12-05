f92819f0f71a3c00a66b83fb7c3ec7534f7f3687
liulong <liulong@liulongdeMacBook-Pro.local>
2020-12-05 19:55:02 +0800

    delete .idea

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -16,7 +16,7 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
-		got := Hello("","")
+		got := Hello("", "")
 		want := "Hello,world"
 		assertCorrectMessage(t, got, want)
 	})
--- repeat/repeat.go
+++ repeat/repeat.go
@@ -1,1 +1,8 @@
 package iteration
+
+func Repeat(character string) (repeated string) {
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+	return
+}
